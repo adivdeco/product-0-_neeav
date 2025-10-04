2de28a1aaeb49b6f1a2f2e4957be3a692e0498b0
adivdeco <sadiv120@gmail.com>
2025-10-04 12:51:48 +0530

    lets start frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+
+    <div className='bg-black text-white'>
+      <h1>heloo</h1>
+    </div>
+  )
+}
+
+export default App
