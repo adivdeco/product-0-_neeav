e65119e6532ee351db27651024afbe66b40131c0
adivdeco <sadiv120@gmail.com>
2025-11-16 20:03:43 +0530

    last

--- backend/src/index.js
+++ backend/src/index.js
@@ -42,8 +42,10 @@ const sessionMiddleware = session({
         httpOnly: true,
         secure: true,
         sameSite: "none",
+        domain: ".onrender.com",
         maxAge: 30 * 24 * 60 * 60 * 1000
     }
+
 
 });
 
