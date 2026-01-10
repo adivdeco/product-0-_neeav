5e2dcba8b7c99133e7c3074fafb279b83960a874
adivdeco <sadiv120@gmail.com>
2026-01-10 14:04:06 +0530

    up

--- frontend/src/api/auth.js
+++ frontend/src/api/auth.js
@@ -5,6 +5,7 @@ import axios from "axios"
 const axiosClient = axios.create({
     // baseURL: 'http://localhost:3001',
     baseURL: import.meta.env.VITE_API_URL,
+    
     withCredentials: true,
     headers: {
         'Content-Type': 'application/json'
